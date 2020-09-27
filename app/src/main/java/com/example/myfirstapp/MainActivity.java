0fd64b84905b06d9490baf3d0f0637a253f0fc69
Raul Paulino <20461484+rfire12@users.noreply.github.com>
2020-09-27 15:25:55 -0400

    add date picker

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -2,24 +2,73 @@ package com.example.myfirstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
 import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView birthdateTextView;
+    private DatePickerDialog.OnDateSetListener birthdateTextViewListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        this.setContentView(R.layout.activity_main);
+        this.addGenderSpinnerOptions();
+        this.createDatePicker();
+    }
+
+    private void addGenderSpinnerOptions() {
+        birthdateTextView = (TextView) findViewById(R.id.editTextDate);
 
         Spinner genderSpinner = (Spinner) findViewById(R.id.spinnerGender);
-
         ArrayAdapter<String> genderAdapter = new ArrayAdapter<String>(MainActivity.this,
                 android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.genders_array));
 
         genderAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         genderSpinner.setAdapter(genderAdapter);
+    }
+
+    private void createDatePicker() {
+
+        birthdateTextView.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar calendar = Calendar.getInstance();
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(
+                        MainActivity.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        birthdateTextViewListener,
+                        year,month,day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+
+        birthdateTextViewListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month + 1;
+                Log.d("MainActivity", "onDateSet: mm/dd/yyy: " + month + "/" + day + "/" + year);
+
+                String date = month + "/" + day + "/" + year;
+                birthdateTextView.setText(date);
+            }
+        };
     }
 }
